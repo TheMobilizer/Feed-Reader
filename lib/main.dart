9317ee08a313ce521f5e07d4d2022669d57b4214
Hashir Sibtain <hashirsibtain@live.com>
2024-10-06 22:50:35 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  final List<String> fruits = ['apple', 'banana', 'grape', 'orange', 'kiwi'];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Reader',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text("Mreader"),
+      ),
+      body: ListView.builder(
+              //padding: const EdgeInsets.all(8),
+              //shrinkWrap: true,
+              itemCount: fruits.length,
+              itemBuilder: (context,index){
+                return(Center(child: Container(color: Colors.deepPurple, width: 100, child: Center(child: Text(fruits[index])))));
+              } 
+            ),
+          )
+        
+      );
+  }
+}
+
+/*class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}*/
